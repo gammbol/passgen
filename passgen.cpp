fabef362b7728adc0270862c7b4492f34701a8d9
Leonid Ushakov <gammbol2525@gmail.com>
2024-10-14 09:34:54 +0300

    update passgen.cpp

--- passgen.cpp
+++ passgen.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+/* Password Generator by gammbol
+ *
+ * Flags:
+ *  -n: use only numberic values
+ *  -v: verbose
+ *  -e: exclude repetitive values
+ *  -l num: specify length
+ *  -t: exlude repetitive values near each other
+*/
+
 int main()
 {
   std::cout << "Hello world!" << std::endl;
